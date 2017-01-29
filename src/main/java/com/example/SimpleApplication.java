7597b271946779f2366e3a9c702227258d1bf1ed
dmitrypashkov <dmitry.pashkov@nextiva.com>
2017-01-29 15:01:40 +0200

    Added login link

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -3,12 +3,37 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+@RestController
 @EnableOAuth2Sso
 @SpringBootApplication
-public class SimpleApplication {
+public class SimpleApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
 	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
+	}
+
+
 }
