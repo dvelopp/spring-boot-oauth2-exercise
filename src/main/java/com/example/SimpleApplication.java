f0aa4f8d8a8501160a1fc3b05df84dc2631ac2c3
dmitrypashkov <dmitry.pashkov@nextiva.com>
2017-01-29 17:52:15 +0200

    Custom error page

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -3,26 +3,62 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 @EnableOAuth2Sso
 @SpringBootApplication
 public class SimpleApplication extends WebSecurityConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SimpleApplication.class, args);
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
+    }
+
+    @Bean
+    public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+        return map -> {
+            String url = (String) map.get("organizations_url");
+            List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+            if (orgs.stream()
+                    .anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+                return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+            }
+            throw new BadCredentialsException("Not in Spring Projects origanization");
+        };
     }
 
     @RequestMapping("/user")
+    @ResponseBody
     public Principal user(Principal principal) {
         return principal;
+    }
+
+    @RequestMapping("/unauthenticated")
+    public String unauthenticated() {
+        return "redirect:/?error=true";
     }
 
     @Override
@@ -36,6 +72,20 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Configuration
+    protected static class ServletCustomizer {
+        @Bean
+        public EmbeddedServletContainerCustomizer customizer() {
+            return container -> {
+                container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
+            };
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
     }
 
 
